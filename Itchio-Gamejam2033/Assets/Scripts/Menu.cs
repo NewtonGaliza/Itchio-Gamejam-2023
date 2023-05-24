03e9e96d2d83099e4a04d99f4cee598fdfc3d10a
Newton Galiza <newtonjgaliza@gmail.com>
2023-05-24 17:52:30 -0300

    commit
    commit

--- Itchio-Gamejam2033/Assets/Scripts/Menu.cs
+++ Itchio-Gamejam2033/Assets/Scripts/Menu.cs
@@ -6,17 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField] private Button buttonAbout;
+   // [SerializeField] private Button buttonAbout;
+  //  [SerializeField] private Button buttonMenu;
 
     private void Awake()
     {
-        buttonAbout.onClick.AddListener(LoadAboutScene);
+        //buttonAbout.onClick.AddListener(LoadAboutScene);
+      //  buttonMenu.onClick.AddListener(LoadStartScene);
     }
 
-    // Update is called once per frame
     void LoadAboutScene()
     {
         SceneManager.LoadScene("About");
+    }
+
+    void LoadStartScene()
+    {
+        SceneManager.LoadScene("StartScene");
     }
 
      
